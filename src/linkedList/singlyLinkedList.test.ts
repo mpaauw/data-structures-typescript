6a9adb945af5ad7b52d2b3c2691712b5c34de4d3
mpaauw <4207462+mpaauw@users.noreply.github.com>
2022-08-16 22:41:16 -0700

    Add initial unit tests for insertAtTail.

--- src/linkedList/singlyLinkedList.test.ts
+++ src/linkedList/singlyLinkedList.test.ts
@@ -1,40 +1,64 @@
 import { SinglyLinkedList } from "./singlyLinkedList";
 import { faker } from '@faker-js/faker';
 
-let singlyLinkedList!: SinglyLinkedList<string>;
+describe('singlyLinkedList_Tests_', () => {
 
-describe('insertAtHead_Tests_', () => {
+  let singlyLinkedList!: SinglyLinkedList<string>;
 
-  beforeEach(() => {
+  beforeAll(() => {
     singlyLinkedList = new SinglyLinkedList<string>();
   });
 
-  test('insertAtHead_InternalError_ThrowError', async () => {
-    // arrange
-    singlyLinkedList['logger'].debug = jest.fn()
-      .mockImplementationOnce(() => {
-        throw new Error();
-      });
-
-    // act / assert
-    expect(() => {
-      singlyLinkedList.insertAtHead(faker.hacker.noun());
-    }).toThrowError();
+  describe('insertAtHead_Tests_', () => {
+  
+    test('insertAtHead_InternalError_ThrowError', async () => {
+      // arrange
+      singlyLinkedList['logger'].debug = jest.fn()
+        .mockImplementationOnce(() => {
+          throw new Error();
+        });
+  
+      // act / assert
+      expect(() => {
+        singlyLinkedList.insertAtHead(faker.hacker.noun());
+      }).toThrowError();
+    });
+  
+    test('insertAtHead_Success_Return', async () => {
+      // arrange
+      const oldHead = singlyLinkedList['head'];
+      const oldSize = singlyLinkedList['size'];
+      const valueToInsert = faker.hacker.noun();
+  
+      // act
+      singlyLinkedList.insertAtHead(valueToInsert);
+  
+      // assert
+      expect(singlyLinkedList['head'].value).toEqual(valueToInsert);
+      expect(singlyLinkedList['head'].next).toEqual(oldHead);
+      expect(singlyLinkedList['size']).toEqual(oldSize + 1);
+    });
+    
   });
 
-  test('insertAtHead_Success_Return', async () => {
-    // arrange
-    const oldHead = singlyLinkedList['head'];
-    const oldSize = singlyLinkedList['size'];
-    const valueToInsert = faker.hacker.noun();
+  describe('insertAtTail_Tests_', () => {
 
-    // act
-    singlyLinkedList.insertAtHead(valueToInsert);
+    test('insertAtTail_InternalError_ThrowError', async () => {
+      // arrange
+      singlyLinkedList['logger'].debug = jest.fn()
+        .mockImplementationOnce(() => {
+          throw new Error();
+        });
 
-    // assert
-    expect(singlyLinkedList['head'].value).toEqual(valueToInsert);
-    expect(singlyLinkedList['head'].next).toEqual(oldHead);
-    expect(singlyLinkedList['size']).toEqual(oldSize + 1);
+      // act / assert
+      expect(() => {
+        singlyLinkedList.insertAtTail(faker.hacker.noun());
+      }).toThrowError();
+    });
+
   });
+
+
 
 });
+
