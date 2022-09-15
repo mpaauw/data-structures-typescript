60706dc8a706b583f99f714aa09b3e829c3bf844
mpaauw <4207462+mpaauw@users.noreply.github.com>
2022-09-15 13:50:13 -0700

    Enforce minimum jest coverage of >=90% across all categories.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
   testEnvironment: 'node',
   globalSetup: '<rootDir>/tests/globalSetup.js',
   testTimeout: 60000,
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branch: 90,
+      functions: 90,
+      lines: 90
+    }
+  }
 };
