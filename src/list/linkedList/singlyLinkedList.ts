7ee7b4bc9088c37e5c811f53b2534875172a0250
mpaauw <4207462+mpaauw@users.noreply.github.com>
2022-09-15 14:36:23 -0700

    feat(hash-table): add initial hash table boilerplate (#6)
    * feat(hash-table): add initial hash table boilerplate
    
    include various cleanup / refactoring
    
    * feat(hashtable): add initial hash function impl
    
    with fixed-quantity, bucket-based hash collision solution
    
    * feat(hashtable): add method to remove entry
    
    fixup various other code
    
    * feat(hashtable): add initial method implementations for contains, get
    
    * Add unit tests for hashtable; fix existing tests, source bugs.
    
    * Enforce minimum jest coverage of >=90% across all categories.
    
    * Fix test bug.
    
    * Lint.

--- src/list/linkedList/singlyLinkedList.ts
+++ src/list/linkedList/singlyLinkedList.ts
@@ -1,20 +1,16 @@
-import { Logger } from '../util/logger';
+import { BaseDataStructure } from '../../shared/baseDataStructure';
 import { SinglyLinkedListNode } from './model/singlyLinkedListNode';
 
 /**
  * Contains common functions for a Singly-Linked List data structure.
  */
-export class SinglyLinkedList<T> {
-  private logger = Logger.createLogger(process.env.ENVIRONMENT === ('local') ? __filename : '');
-
+export class SinglyLinkedList<T> extends BaseDataStructure {
   private head!: SinglyLinkedListNode<T>;
 
   private tail!: SinglyLinkedListNode<T>;
 
-  private size!: number;
-
   public constructor() {
-    this.size = 0;
+    super(__filename);
   }
 
   /**
@@ -124,7 +120,7 @@ export class SinglyLinkedList<T> {
       }
 
       let iterator = this.head;
-      for (let i = 0; i < index; i++) {
+      for (let i = 0; i < index - 1; i++) {
         iterator = iterator.next;
       }
       const newNode = new SinglyLinkedListNode<T>();
@@ -162,21 +158,15 @@ export class SinglyLinkedList<T> {
         currentListState: this,
       });
 
-      let iterator = this.head;
-      for (let i = 0; i < this.size - 1; i++) {
-        if (iterator.value === value) {
-          this.logger.debug('Successfully found value in Singly-Linked List.', {
-            valueToFind: value,
-            foundNode: iterator,
-            currentListState: this,
-          });
-
-          return iterator;
+      let iteratorNode = this.head;
+      while (iteratorNode != null) {
+        if (iteratorNode.value === value) {
+          return iteratorNode;
         }
-        iterator = iterator.next;
+        iteratorNode = iteratorNode.next;
       }
 
-      this.logger.warn('Unable to find value in Singly-Linked List; returning null.', {
+      this.logger.warn('Unable to find value in Singly-Linked List; returning undefined.', {
         valueToFind: value,
         currentListState: this,
       });
@@ -289,17 +279,21 @@ export class SinglyLinkedList<T> {
           currentListState: this,
         });
         return;
+      } if (this.size === 1) {
+        this.tail = undefined;
+        this.head = undefined;
+      } else {
+        let iterator = this.head;
+        for (let i = 0; i < this.size - 1; i++) {
+          if (iterator.next === this.tail) {
+            break;
+          }
+          iterator = iterator.next;
+        }
+        iterator.next = null;
+        this.tail = iterator;
       }
 
-      let iterator = this.head;
-      for (let i = 0; i < this.size - 1; i++) {
-        if (iterator.next === this.tail) {
-          break;
-        }
-        iterator = iterator.next;
-      }
-      iterator.next = null;
-      this.tail = iterator;
       this.size -= 1;
       this.updateTail(this.tail);
 
@@ -375,8 +369,22 @@ export class SinglyLinkedList<T> {
    * @param node to check for tail reference.
    */
   private updateTail(node: SinglyLinkedListNode<T>): void {
-    if (node.next == null) {
-      this.tail = node;
+    if (this.size === 2 && this.head.next != null) { // handle base case at low list sizes
+      this.tail = this.head.next;
     }
+
+    if (node != null) {
+      if (node.next === null || node.next === undefined) {
+        this.tail = node;
+      }
+    }
+  }
+
+  /**
+   * Simple method that returns the head node of the List for traversal purposes.
+   * @returns head of Singly-Linked List.
+   */
+  public getHead(): SinglyLinkedListNode<T> {
+    return this.head;
   }
 }
