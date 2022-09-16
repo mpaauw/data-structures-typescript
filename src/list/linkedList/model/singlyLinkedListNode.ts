f10eb50923f1b98d3838879e854fff8f9286d8c7
mpaauw <4207462+mpaauw@users.noreply.github.com>
2022-09-16 11:56:26 -0700

    Add stack implementation; unit tests; fix various other bugs; cleanup.

--- src/list/linkedList/model/singlyLinkedListNode.ts
+++ src/list/linkedList/model/singlyLinkedListNode.ts
@@ -2,4 +2,12 @@ export class SinglyLinkedListNode<T> {
   public value!: T;
 
   public next!: SinglyLinkedListNode<T>;
+
+  public constructor(
+    value?: T,
+    next?: SinglyLinkedListNode<T>,
+  ) {
+    this.value = value;
+    this.next = next;
+  }
 }
---
+++ src/stack/stack.test.ts
@@ -0,0 +1,199 @@
+import { Stack } from "./stack";
+import { faker } from '@faker-js/faker';
+
+describe('stack_Tests_', () => {
+
+    let stack!: Stack<string>;
+
+    beforeEach(() => {
+        stack = new Stack<string>();
+    });
+
+    describe('push_Tests_', () => {
+
+        test('push_CatchError_ThrowError', () => {
+            // arrange
+            jest.spyOn(stack, 'isEmpty')
+                .mockImplementationOnce(() => {
+                    throw new Error();
+                });
+
+            // act / assert
+            expect(() => {
+                stack.push(faker.hacker.noun());
+            }).toThrowError();
+        });
+
+        test('push_EmptyStack_PushNodeToTop', () => {
+            // arrange
+            const oldSize = stack['size'];
+            const valueToPush = createUniqueValue();
+
+            // act
+            stack.push(valueToPush);
+
+            // assert
+            expect(oldSize).toEqual(0);
+            expect(stack['size']).toEqual(oldSize + 1);
+            expect(stack['top'].value).toEqual(valueToPush);
+        }); 
+
+        test('push_NonEmptyStack_PushNodeToTop', () => {
+            // arrange
+            populateStack();
+            const oldSize = stack['size'];
+            const oldTopNode = stack['top'];
+            const valueToPush = createUniqueValue();
+
+            // act
+            stack.push(valueToPush);
+
+            // assert
+            expect(oldSize).toBeGreaterThan(0);
+            expect(stack['size']).toEqual(oldSize + 1);
+            expect(stack['top']).not.toEqual(oldTopNode);
+            expect(stack['top'].next).toEqual(oldTopNode);
+            expect(stack['top'].value).toEqual(valueToPush);
+        });
+
+    });
+
+    describe('pop_Tests_', () => {
+        
+        test('pop_CatchError_ThrowError', () => {
+            // arrange
+            jest.spyOn(stack, 'isEmpty')
+                .mockImplementationOnce(() => {
+                    throw new Error();
+                });
+
+            // act / assert
+            expect(() => {
+                stack.pop();
+            }).toThrowError();
+        }); 
+
+        test('pop_EmptyStack_ReturnUndefined', () => {
+            // arrange
+            const oldSize = stack['size'];
+
+            // act
+            const result = stack.pop();
+
+            // assert
+            expect(oldSize).toEqual(0);
+            expect(stack['size']).toEqual(oldSize);
+            expect(result).toBeUndefined();
+        });
+
+        test('pop_NonEmptyStack_ReturnTopNodeValue', () => {
+            // arrange
+            populateStack();
+            const oldSize = stack['size'];
+            const oldTopNode = stack['top'];
+
+            // act
+            const result = stack.pop();
+
+            // assert
+            expect(oldSize).toBeGreaterThan(0);
+            expect(stack['size']).toEqual(oldSize - 1);
+            expect(oldTopNode).toBeDefined();
+            expect(stack['top']).not.toEqual(oldTopNode);
+            expect(stack['top']).toEqual(oldTopNode.next);
+        }); 
+    });
+
+    describe('peek_Tests_', () => {
+
+        test('peek_CatchError_ThrowError', () => {
+            // arrange
+            jest.spyOn(stack, 'isEmpty')
+                .mockImplementationOnce(() => {
+                    throw new Error();
+                });
+
+            // act / assert
+            expect(() => {
+                stack.peek();
+            }).toThrowError();
+        });
+
+        test('peek_EmptyStack_ReturnUndefined', () => {
+            // arrange
+            const oldSize = stack['size'];
+
+            // act
+            const result = stack.peek();
+
+            // assert
+            expect(oldSize).toEqual(0);
+            expect(result).toBeUndefined();
+        });
+
+        test('peek_NonEmptyStack_ReturnTopNodeValue', () => {
+            // arrange
+            populateStack();
+            const size = stack['size'];
+            const topNode = stack['top'];
+
+            // act
+            const result = stack.peek();
+
+            // assert
+            expect(size).toBeGreaterThan(0);
+            expect(topNode).toBeDefined();
+            expect(stack['top']).toEqual(topNode);
+        });
+    });
+
+    describe('isEmpty_Tests_', () => {
+
+        test('isEmpty_CatchError_ThrowError', () => {
+            // arrange
+            stack = null;
+
+            // act / assert
+            expect(() => {
+                stack.isEmpty();
+            }).toThrowError();
+        });
+
+        test('isEmpty_EmptyStack_ReturnTrue', () => {
+            // arrange
+            const size = stack['size'];
+
+            // act
+            const result = stack.isEmpty();
+
+            // assert
+            expect(size).toEqual(0);
+            expect(result).toBeTruthy();
+        });
+
+        test('isEmpty_NonEmptyStack_ReturnFalse', () => {
+            // arrange
+            populateStack();
+            const size = stack['size'];
+
+            // act
+            const result = stack.isEmpty();
+
+            // assert
+            expect(size).toBeGreaterThan(0);
+            expect(result).toBeFalsy();
+        });
+
+    });
+
+    const createUniqueValue = (): string => {
+        return faker.hacker.noun() + faker.animal.dog() + new Date().getTime();
+    }
+
+    const populateStack = (numberOfNodes: number = 10): void => {
+        for(let i = 0; i < numberOfNodes; i++) {
+            stack.push(createUniqueValue());
+        }
+    }
+
+});
